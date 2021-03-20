e079918682698a69269e8d1a6547083b3f4ca8dd
gyudong <zlsrbrhd@gmail.com>
2021-03-20 18:59:34 +0900

    Fix : mypage/views.py 수정

--- mypage/views.py
+++ mypage/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import json
+import bcrypt
+import jwt
 
-# Create your views here.
+from django.views import View
+from django.http import JsonResponse
+
+from product.models import Product
+from mypage.models import Favorite
+
+@decorator
+class Favorite(View):
+def post(self, request):
+data = json.loads(request.body)
+
+if Favorite.objects.filter(id=request.user_id).exists():
+Favorite.objects.get(id=request.user_id)
+
