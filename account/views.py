fb32d2a2835824bddea623fbc04288e0f0587134
gyudong <zlsrbrhd@gmail.com>
2021-03-20 16:31:08 +0900

    Add : utils.py 추가 후 데코레이터 작성 / Fix : views.py 예외처리 수정

--- account/views.py
+++ account/views.py
@@ -18,12 +18,12 @@ class UserSignUp(View):
             name         = data['name']
             phone_number = data['phone_number']
             email        = data['email']
-
+            
             if not login_id.isalnum():
                 return JsonResponse({"message":"No Special Characters."}, status = 400)
             
             if User.objects.filter(login_id=login_id).exists():
-                return JsonResponse({"message":"Already registered login_id, Please put another login_id."}, status = 400)
+                return JsonResponse({"message":"USER_NAME_ALREADY_EXITS"}, status = 400)
 
             if len(password) < 8:
                 return JsonResponse({"message":"More than 8 letters PLEASE."}, status = 400)
@@ -37,7 +37,11 @@ class UserSignUp(View):
             
             User.objects.create(login_id=login_id, password=password_hashed, name=name, phone_number=phone_number, email=email)
 
-            return JsonResponse({'message':'SUCESS'}, status = 200)
+            u1 = User.objects.get(login_id=login_id).is_active
+            u1 = True
+            u1.save()
+
+            return JsonResponse({'message':'SUCCESS'}, status = 200)
         
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status = 400)
@@ -56,18 +60,27 @@ class UserSignIn(View):
             if User.objects.filter(login_id=login_id).exists():
                 user_password = User.objects.get(login_id=login_id).password
                 user_id       = User.objects.get(login_id=login_id).id
-                user_auth     = User.objects.get(login_id=login_id).is_active
+                user_active     = User.objects.get(login_id=login_id).is_active
 
                 if bcrypt.checkpw(password.encode('utf-8'), user_password.encode('utf-8')):
-                    access_token = jwt.encode({'id':user_id, 'auth':user_auth}, SECRET_KEY, ALGORITHM)
-                    return JsonResponse({"message":"SUCCESS", 'access_token':access_token}, status = 200)
+                    if user_active:
+                        access_token = jwt.encode({'id':user_id}, SECRET_KEY, ALGORITHM)
+                        return JsonResponse({"message":"SUCCESS", 'access_token':access_token}, status = 200)
+                    else:
+                        return JsonResponse({"message":"please inzoong"}, status = 400)
                 else:
                     return JsonResponse({"message":"INVALID_USER"}, status = 401)    
             else:
                 return JsonResponse({"message":"INVALID_USER"}, status = 401)
             
+        except DoesNotExist:
+            return JsonResponse({"message":"djqtek"}, status = 400)
+
+        except MultipleObjectsReturned:
+            return JsonResponse({"message":"aksgek"}, status = 400)
+        
         except KeyError:
-                return JsonResponse({"message":"KEY_ERROR"}, status = 400)
+            return JsonResponse({"message":"KEY_ERROR"}, status = 400)
         
         except json.decoder.JSONDecodeError:
             return JsonResponse({"message": "json.decoder.JSONDecodeError"}, status = 400)
