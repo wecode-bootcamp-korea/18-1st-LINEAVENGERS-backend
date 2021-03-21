7f50060061a90d0210083cc06de9bbff46c4270b
parkgood1020 <parkgood1020@gmail.com>
2021-03-21 10:30:02 +0900

    MODIFY : account, order app models.py modify

--- account/models.py
+++ account/models.py
@@ -9,12 +9,6 @@ class User(models.Model):
     email        = models.CharField(max_length=50)
     image_url    = models.URLField(max_length=2000, null=True)
     is_active    = models.BooleanField(default=False)
-    order_place  = models.ManyToManyField(
-        'order.Address',
-        through='order.Order',
-        through_fields=('user', 'address'),
-        related_name='place_users',
-    )
     
     class Meta:
         db_table = "users"
