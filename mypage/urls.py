e00afc711b41646834937583e24a7d824a9e8649
gyudong <zlsrbrhd@gmail.com>
2021-03-22 13:43:22 +0900

    Add : 찜 기능 추가

--- mypage/urls.py
+++ mypage/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import Favorite 
+from .views import FavoriteView
 
 urlpatterns = [
-    path('/favorite', Favorite.as_view())
+    path('/favorite', FavoriteView.as_view()),
 ]
--- mypage/views.py
+++ mypage/views.py
@@ -5,19 +5,23 @@ import jwt
 from django.views import View
 from django.http import JsonResponse
 
+from account.models import User
 from product.models import Product
-from mypage.models import Favorite
+from mypage.models  import Favorite, Review
+from order.models   import Order
 
-@decorator
-class Favorite(View):
+# @decorator
+class FavoriteView(View):
     def post(self, request):
         data = json.loads(request.body)
         
+        user    = data['user']
+        user    = User.objects.get(id=user)
         product = data['product']
         product = Product.objects.get(id=product)
 
-        if Favorite.objects.filter(id=request.user_id).exists():
-            favorite_user = Favorite.objects.get(id=request.user_id)
+        if Favorite.objects.filter(user_id=user.id).exists():
+            favorite_user = Favorite.objects.get(id=user.id)
 
             if favorite_user.is_favorite:
                 favorite_user.is_favorite = False
@@ -32,8 +36,19 @@ class Favorite(View):
         
         Favorite.objects.create(
             is_favorite=True,
-            user=request.user,
+            user=user,
             product=product,
         )
 
         return JsonResponse({'message':'SUCCESS'}, status = 200)
+
+class ReviewCreate(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        content = data['content']
+        rating  = data['rating']
+        oder    = data['order']
+        order   = Order.objects.get(id=order)
+
+        Review.objects.create(content=content, rating=rating, order=order)
