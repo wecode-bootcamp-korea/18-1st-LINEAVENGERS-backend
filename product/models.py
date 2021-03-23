992974a1af990fd27b9d4c1b517f312072a8a472
parkgood1020 <parkgood1020@gmail.com>
2021-03-23 13:24:35 +0900

    MODIFY : product modesl.py 수정

--- product/models.py
+++ product/models.py
@@ -26,8 +26,7 @@ class Product(models.Model):
     price            = models.DecimalField(max_digits=10, decimal_places=2)
     create_at        = models.DateTimeField(auto_now_add=True)
     update_at        = models.DateTimeField(auto_now=True)
-    is_best          = models.BooleanField(default=False)
-    is_new           = models.BooleanField(default=True)
+    type             = models.CharField(max_length=20)
     is_free_shipping = models.BooleanField(default=False)
     is_soldout       = models.BooleanField(default=False)
     discount_rate    = models.DecimalField(max_digits=5, decimal_places=2)
@@ -62,6 +61,7 @@ class Product(models.Model):
         through_fields=('product', 'user'),
         related_name='reviewed_products',
     )
+
     class Meta:
         db_table = "products"
 
