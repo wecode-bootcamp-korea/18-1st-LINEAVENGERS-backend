65e5a1ddeb34e5f26546ce50cac71b211d16db65
parkgood1020 <parkgood1020@gmail.com>
2021-03-23 21:45:18 +0900

    MODIFY : manage.py migration complete

--- product/models.py
+++ product/models.py
@@ -32,10 +32,10 @@ class Product(models.Model):
     price            = models.DecimalField(max_digits=10, decimal_places=2)
     create_at        = models.DateTimeField(auto_now_add=True)
     update_at        = models.DateTimeField(auto_now=True)
-    type             = models.CharField(max_length=20)
     is_free_shipping = models.BooleanField(default=False)
     is_soldout       = models.BooleanField(default=False)
     discount_rate    = models.DecimalField(max_digits=5, decimal_places=2)
+    type             = models.ForeignKey(Type, on_delete=models.CASCADE, null=True)
     category         = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     sizes            = models.ManyToManyField(
         Size,
