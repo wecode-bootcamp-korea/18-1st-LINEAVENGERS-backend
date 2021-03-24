c23246ca977ebe172160a627160df587ed26cc7c
gyudong <zlsrbrhd@gmail.com>
2021-03-24 11:44:12 +0900

    Modify : mypage/views.py 수정 후 디비 민 후 마이그래이션

--- mypage/views.py
+++ mypage/views.py
@@ -15,13 +15,11 @@ class FavoriteView(View):
     def post(self, request):
         data = json.loads(request.body)
         
-        user    = data['user']
-        user    = User.objects.get(id=user)
         product = data['product']
         product = Product.objects.get(id=product)
 
-        if Favorite.objects.filter(user_id=user.id).exists():
-            favorite_user = Favorite.objects.get(id=user.id)
+        if Favorite.objects.filter(user=request.user).exists():
+            favorite_user = Favorite.objects.get(id=request.user.id)
 
             if favorite_user.is_favorite:
                 favorite_user.is_favorite = False
@@ -41,40 +39,20 @@ class FavoriteView(View):
 
         return JsonResponse({'message':'SUCCESS', }, status = 200)
 
-    # @token_decorator
+    @token_decorator
     def get(self, request):
-        favorites = Favorite.objects.filter(user_id='1', is_favorite=True)
+        favorites = Favorite.objects.filter(user=request.user, is_favorite=True)
         
-        result = [{'name':favorite.product.name, 'price':favorite.product.price, 'image':list(favorite.product.productimage_set.all().values())} for favorite in favorites]
+        result = [{
+            'name':favorite.product.name,
+            'price':favorite.product.price,
+            'image':list(favorite.product.productimage_set.all().values())
+        } for favorite in favorites]
         
-        # result = [product = Product.objects.get(id=favorite_product['product'])
-        # {'name':product.name, 'price':product.price, 'image':ProductImage.objects.get()} 
-        # for favorite_product in favorite_products]
-        # for favorite in favorites:
-        #     favorite_dict = {
-        #         'product' : favorite.product,
-        #     }
-
-        #     favotrite_product.append(favorite_dict)
-        
-        # result = []
-        # print('=============================================', favorite_products)
-
-        # for i in favorite_products:
-        #     product = Product.objects.get(i['product'])
-            
-        #     k = {
-        #         'name' : product.name,
-        #         'price' : product.price,
-        #         'image' : ProductImage.objects.get(product=product).image_url,
-        #     }
-
-        #     result.append(k)
-
         return JsonResponse({"result":result}, status = 200)
-            
+        
 class ReviewView(View):
-    # @token_decorator
+    @token_decorator
     def post(self, request):
         data = json.loads(request.body)
 
@@ -82,6 +60,6 @@ class ReviewView(View):
         rating  = data['rating']
         product = Product.objects.get(id=data['product'])
 
-        Review.objects.create(content=content, rating=rating, product=product, user_id='2')
+        Review.objects.create(content=content, rating=rating, product=product, user=request.user)
 
         return JsonResponse({"message":"SUCCESS"}, status = 200)
