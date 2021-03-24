ae9a9759edc50c9e9d5c8e2c693220fa23885e6b
gyudong <zlsrbrhd@gmail.com>
2021-03-24 20:07:07 +0900

    Add : order/views.py 수정

--- order/views.py
+++ order/views.py
@@ -13,11 +13,29 @@ from order.models  import Cart, Order
 class Cart(View):                           # 이름 고쳐야함
     @token_decorator
     def get(self, request):
-        # orders = request.user.order_set.filter(order_status=1)
-        orders = Order.objects.filter(user=request.user, order_status_id=1)
+        order = Order.objects.get(user=request.user, order_status_id=1)
 
-        order_cart = [order for order in orders]
+        k = order.cart_set.all()
 
-        for orde in order_cart:
-            Cart.objects.filter(order=orde) 
+        # for i in k:
+        #     my_dict = {'name':i.product.name 
+
+        # result = [{
+        #     'name':i.product.name,
+        #     'price':i.product.price,
+        #     'image':i.product.productimage_set.filter(is_thumbnail=True).image_url,
+        #     'quantity':i.quantity,
+        #     'size':i.size.name,
+        #     'deliveryprice':i.product.
+        # } for i in k]
+
+        for i in k:
+            if i.product.is_free_shipping:
+                deliveryprice:0
+            
+
+            
+
+        
+
         
