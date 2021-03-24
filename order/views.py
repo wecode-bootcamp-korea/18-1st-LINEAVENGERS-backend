a917e50aa6c25b761681ca2921e6f0100aa59f3b
gyudong <zlsrbrhd@gmail.com>
2021-03-24 17:47:40 +0900

    Add : order/views.py 장바구니 조회 생성

--- order/views.py
+++ order/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import jwt
+import json
+import bcrypt
 
-# Create your views here.
+from django.views import View
+from django.http  import JsonResponse
+
+from .models       import User
+from my_settings   import SECRET_KEY, ALGORITHM, EMAIL
+from account.utils import token_decorator
+from order.models  import Cart, Order
+
+class Cart(View):                           # 이름 고쳐야함
+    @token_decorator
+    def get(self, request):
+        # orders = request.user.order_set.filter(order_status=1)
+        orders = Order.objects.filter(user=request.user, order_status_id=1)
+
+        order_cart = [order for order in orders]
+
+        for orde in order_cart:
+            Cart.objects.filter(order=orde) 
+        
