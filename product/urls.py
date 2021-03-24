a2258756d01b65be3afcad20dd746c7e38cf2246
parkgood1020 <parkgood1020@gmail.com>
2021-03-24 16:51:16 +0900

    MODIFY : product urls.py modify

--- product/urls.py
+++ product/urls.py
@@ -14,7 +14,6 @@ from product.views import (
 urlpatterns = [
     path('', ProductListView.as_view()),
     path('/main-category', MainCategoryView.as_view()), 
-#   path('/sub-category/<int:menu_id>', SubCategoryView.as_view()), 
     path('/main-product', MainProductView.as_view()),
     path('/detail/<int:product_id>', ProductDetailView.as_view()),
     path('/<int:product_id>/review', ProductReviewView.as_view()),
